9e22b7b118188e849ac72ff9ff728a4e8103e65b
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-01-30 10:45:54 +0530

    completed

--- ControlStructure/Voter.cpp
+++ ControlStructure/Voter.cpp
@@ -7,7 +7,7 @@ int age;
 cout << "Enter Your Age : ";
 cin >> age;
 if(age<=0){
-	cout << "Enter A Valid Age" ;
+	cout << "Don't enter negative number and 0" ;
 }
 else if(age >=18 && age<=100){
 	cout << "Your age is eligible for vote";
