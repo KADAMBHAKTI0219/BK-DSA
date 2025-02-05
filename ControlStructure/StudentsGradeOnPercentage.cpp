601d86fc12469b172681b24db77909f793e62144
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-02-05 07:28:39 +0530

    completed electricitybillAndMax_MIn minwith2 no.

--- ControlStructure/StudentsGradeOnPercentage.cpp
+++ ControlStructure/StudentsGradeOnPercentage.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
 //	WAP TO PRINT GRADES OF STUDENT BASED ON THEIR PERCENTAGE.
---
+++ ElectricityBillAndMax_Min/Minwith2numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num1,num2;
+	cout << "Enter Num1 and Num2 : ";
+	cin >> num1 >> num2;
+	if(num1<num2){
+		cout << "Num1 is Minimum";
+	}
+	else{
+		cout << "Num1 is Minimum";
+	}
+	return 0;
+}
