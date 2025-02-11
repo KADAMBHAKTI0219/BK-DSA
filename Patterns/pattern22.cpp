215e34c5edf2c9f635c07d9171ed6612f6e2833c
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-02-11 22:11:04 +0530

    completed patterns

---
+++ Patterns/pattern22.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char alpha = 'A';
+	for(char i='E';i>='A';i--){
+		for(char j='E';j>=i;j--){
+	
+			cout << alpha << " ";
+			alpha ++;
+		}
+		cout << endl;
+	}
+	
+	return 0;
+}
